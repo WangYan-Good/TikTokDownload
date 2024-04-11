931793fc1776af625c8f6cc2c7764a53ec9da20c
userid <userid@localhost.localdomain>
2024-04-11 13:48:49 -0400

    add main.py

--- Main.py
+++ Main.py
@@ -38,27 +38,34 @@ class Conf ():
         # add section name into list and make sure the name is unque
         if self.section.count(SiftedContext.groups()[0]) == 0:
           self.section.append(SiftedContext.groups()[0])
+          SectionIndex = self.section.index(SectionName)
+
+          if (len(self.url_list) < )
           self.url_list.append(list())
         continue
       
       # match url
       Url = re.match(URL_REQULAR, line)
       if Url is not None:
-        SectionIndex = self.section.index(SectionName)
 
         # append url into self section list
         if len(self.url_list) < SectionIndex:
           print("url list len: {url_list_len} section[{section_name}] index: {section_index}".format(url_list_len=len(self.url_list), section_name=SectionName, section_index=SectionIndex))
           continue
 
+        print(len(self.url_list[SectionIndex]))
         if len(self.url_list[SectionIndex]) == 0:
-          list(self.url_list[SectionIndex]).append(str())
-
-        if list(self.url_list[SectionIndex]).count(Url.groups()[0]) == 0:
-          list(self.url_list[SectionIndex]).append(Url.groups()[0])
+          list(self.url_list[SectionIndex]).append("")
+        
+        # print(Url)
+        # print(len(self.url_list[SectionIndex]))
+        print(Url[0])
+        if list(self.url_list[SectionIndex]).count(Url[0]) == 0:
+          list(self.url_list[SectionIndex]).append(Url[0])
           continue
 
-    print(self.self.url_list)
+    # print(self.section)
+    # print(self.url_list)
   
   def getPostList(self):
     pass
